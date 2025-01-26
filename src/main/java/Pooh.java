c6978452e00bc658a4a60af1050b7df3be8e8c19
Travis Eswaran <>
2025-01-26 15:36:25 +0800

    Rename, greet and exit

---
+++ src/main/java/Pooh.java
@@ -0,0 +1,9 @@
+public class Pooh {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Pooh, your own personal assistant");
+        System.out.println("What can I do for you today sir?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
